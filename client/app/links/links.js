07c76acf9db27a9d70fe29d4c2a5942e4a093f07
josh-stevens <jcstevens12@outlook.com>
2015-08-28 17:02:32 -0500

    Crushing it

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,13 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+  $scope.data = { links: []};
+  $scope.getLinks = function() {
+    Links.getLinks()
+     .then(function(data){
+       $scope.data.links = data;
+     })
+      };
+
+  $scope.getLinks();
 });
